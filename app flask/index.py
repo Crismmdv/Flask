aa6cc84bcf865845f6c490b23c57e268960d9e81
Cristobal Machuca <crismacdv@gmail.com>
2022-07-20 20:16:26 -0400

    cargar lista en tabla

--- app flask/index.py
+++ app flask/index.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from contextlib import redirect_stderr
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_wtf import FlaskForm
+#from requests import request as rqt
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
 import pandas as pd
+from funciones import creardf_sc
 
 
 
@@ -34,12 +37,35 @@ def Datos():
     form= UploadFileForm()
     if form.validate_on_submit():
         file= form.file.data
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
-        #tit=pd.read_csv(file,',')
-        #titulos=tit
-        return render_template('carga.html',tabla=file)
+        ruta3= os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))
+        ruta2= os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename('archivo.csv'))
+        file.save(ruta3)
+        tit= creardf_sc(pd.read_csv(ruta3,','))
+        
+        titulos= tit.to_csv(ruta2)
+        session["ruta"]=ruta2
+        return redirect(url_for('Tablas'))
 
     return render_template('Datos.html',form=form)
+@app.route('/Tablas', methods=['GET','POST'])
+def Tablas():
+    elementos=('Cl','SO4','HCO3','CO3','Na','Ca','Mg')
+    if "ruta" in session:
+        ruta=session["ruta"] 
+        try: titulos = pd.read_csv(ruta)
+        except: titulos=''
+    else: titulos='error'
+    if request.method =='POST':
+        tg= request.form['tipog']
+        
+        next = tg!=''
+        print (next)
+        if next:
+            return render_template('carga.html',tabla=titulos,elem=elementos,cabecera='Parámetros '+tg)
+        return render_template('carga.html',tabla=titulos,elem=elementos,cabecera="Error")
+    return render_template('cargapiper.html',tabla=titulos,elem=elementos)   
+
+
 @app.route('/Visor')
 def Visor():
     graficos=("Schoeller","Piper", "Stiff")
